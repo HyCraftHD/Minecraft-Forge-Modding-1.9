5e5738d80f7b4725bbe89a8bb8f75cba5d7a6f15
HyCraftHD <hycrafthd@live.de>
2016-03-25 14:58:52 +0100

    Video 1-4

---
+++ src/main/java/net/hycrafthd/youtubetut/TutorialMod.java
@@ -0,0 +1,38 @@
+package net.hycrafthd.youtubetut;
+
+import net.hycrafthd.youtubetut.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@Mod(modid = TutorialMod.MODID, version = TutorialMod.VERSION)
+public class TutorialMod {
+
+	public static final String MODID = "tutorial";
+	public static final String VERSION = "0.1";
+
+	@Instance(MODID)
+	public static TutorialMod INSTANCE = new TutorialMod();
+
+	@SidedProxy(serverSide = "net.hycrafthd.youtubetut.proxy.CommonProxy", clientSide = "net.hycrafthd.youtubetut.proxy.ClientProxy", modId = MODID)
+	public static CommonProxy PROXY = new CommonProxy();
+
+	@EventHandler
+	public void preinit(FMLPreInitializationEvent event) {
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@EventHandler
+	public void postinit(FMLPostInitializationEvent event) {
+		PROXY.registerModels();
+	}
+}
---
+++ src/main/java/net/hycrafthd/youtubetut/proxy/CommonProxy.java
@@ -0,0 +1,9 @@
+package net.hycrafthd.youtubetut.proxy;
+
+public class CommonProxy {
+	
+	//Client Stuff
+	public void registerModels() {
+	}
+
+}
